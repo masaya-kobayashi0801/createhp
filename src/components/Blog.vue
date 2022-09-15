a0075b67021ee8b2c13a9631313b809eec524deb
masayakobayashi <msy.kobayashi5401@gmail.com>
2022-09-15 14:03:20 +0900

    install_axios

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,19 +1,57 @@
 <template>
   <section class="blog">
     <AppBackgroundHolder :title="title" />
+    <p>
+      Ask a yes/no quetion:
+    <input v-model="question"/>
+    </p>
+    <p>{{info}}</p>
+    <p>{{error}}</p>
+    <p>{{message}}</p>
   </section>
 </template>
 <script>
+// import { watch } from 'fs';
 import AppBackgroundHolder from './AppBackgroundHolder.vue'
+import axios from 'axios'
 
 export default {
   data () {
     return {
-      title: 'Blog'
+      title: 'Blog',
+      quetion: '',
+      info: '',
+      error: '',
+      message: '',
+      answer: 'Questions usually contain a question mark. ;-)'
     }
   },
   components: {
     AppBackgroundHolder
+  },
+  watch: {
+    quetion (newQuetion, oldQuetion) {
+      if (newQuetion.includes('?')) {
+        this.getAnswer()
+      }
+    }
+  },
+  mounted () {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => (this.info = response.data.time))
+    .catch(e => this.error = e)
+    .finally(msg => this.message = {title: 'finallyを実行しました',message: msg})
+  },
+  methods: {
+    async getAnswer () {
+      this.answer = 'Thinking...'
+      try {
+        const res = await fetch('https://yesno.wtf/api')
+        this.answer = (await res.json()).answer
+      } catch (error) {
+        this.answer = 'Error! Could not reach the API. ' + error
+      }
+    }
   }
 }
 </script>
